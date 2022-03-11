270d4327fd57c84d9004d0198ce7687a702dc66a
Your Namegit <you@example.com>
2022-03-11 19:49:19 -0300

    Sim

--- resources/views/app/autoridades.blade.php
+++ resources/views/app/autoridades.blade.php
@@ -24,6 +24,8 @@
                     <br>
                     <input name="nome" type="text" placeholder="Nome" class="borda-preta">
                     <br>
+                    <input name="foto" type="text" placeholder="Foto" class="borda-preta">
+                    <br>
                     <input name="representando" type="text" placeholder="Representando?" class="borda-preta">
                     <br>
                     <select name="cargo_principal" class="borda-preta">
@@ -34,8 +36,6 @@
                     </select>
                     <br>
                     <input name="nome_principal" type="text" placeholder="Nome Principal" class="borda-preta">
-                    <br>
-                    <input name="foto" type="text" placeholder="Foto" class="borda-preta">
                     <br>
                     <button type="submit" class="borda-preta">Cadastrar</button>
                 </form>
